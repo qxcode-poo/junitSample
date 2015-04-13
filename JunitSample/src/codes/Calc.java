8c74b730f789126740f696939f47755d3947ed21
David Sena <sena.ufc@gmail.com>
2015-04-13 17:01:56 -0300

    Primeiro commit

---
+++ JunitSample/src/codes/Calc.java
@@ -0,0 +1,26 @@
+package codes;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class Calc implements ICalc {
+
+	@Override
+	public int sum(int a, int b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<Integer> umAteN(int n) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getMaior(Iterator<Integer> it) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
